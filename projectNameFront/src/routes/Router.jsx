86f1c43f7fce1b3b3120ee9a22fb0fe7960515c6
yoobin kim <yoobin0821@naver.com>
2025-07-14 12:25:30 +0900

    회의 후 수정

--- projectNameFront/src/routes/Router.jsx
+++ projectNameFront/src/routes/Router.jsx
@@ -1,11 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LoginPage from '../pages/LoginPage';
+import MainPage from '../pages/MainPage';
+
 
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/scops/main" element={<LoginPage />} />
+        <Route path="/mainPage" element={<MainPage />} />
       </Routes>
     </BrowserRouter>
   );
