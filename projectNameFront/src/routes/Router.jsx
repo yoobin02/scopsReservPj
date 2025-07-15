185bf2ff751d02854b7da2ebbf0b42ce50853d9b
yoobin kim <yoobin0821@naver.com>
2025-07-15 14:38:14 +0900

    250715

--- projectNameFront/src/routes/Router.jsx
+++ projectNameFront/src/routes/Router.jsx
@@ -1,14 +1,16 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LoginPage from '../pages/LoginPage';
 import MainPage from '../pages/MainPage';
+import ReservationPage from '../pages/ReservationPage';
 
 
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/scops/main" element={<LoginPage />} />
-        <Route path="/mainPage" element={<MainPage />} />
+        <Route path="/scops/login" element={<LoginPage />} />
+        <Route path="/scops/main" element={<MainPage />} />
+        <Route path="/scops/reservation" element={<ReservationPage />} />
       </Routes>
     </BrowserRouter>
   );
