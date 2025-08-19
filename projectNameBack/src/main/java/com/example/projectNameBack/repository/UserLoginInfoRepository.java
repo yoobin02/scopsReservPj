7c2e6c3ebf1a116aec87fecdc7cd8bd93a6ca861
yoobin kim <yoobin0821@naver.com>
2025-08-19 15:48:13 +0900

    250819

--- projectNameBack/src/main/java/com/example/projectNameBack/repository/UserLoginInfoRepository.java
+++ projectNameBack/src/main/java/com/example/projectNameBack/repository/UserLoginInfoRepository.java
@@ -4,6 +4,9 @@ import com.example.projectNameBack.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserLoginInfoRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUserID(String userID);
 }
